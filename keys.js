fbe1bd8588e715347b3d81efce7f1d6d4d77b61e
danyel oliveira <oliveiradanyel901@gmail.com>
2019-08-24 22:53:24 -0600

    spotfy bands in tow and omdb apis working

--- keys.js
+++ keys.js
@@ -4,3 +4,10 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+
+exports.OMDB_ID = {
+  id: process.env.OMDB_ID,
+};
+
+
--- liri.js
+++ liri.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 
-var request = require("request");
+var moment = require("moment");
 var fs = require("fs");
+var axios = require("axios");
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
@@ -21,10 +22,10 @@ function UserInputs(userOption, inputParameter) {
       showSongsInfo(inputParameter);
       break;
     case "movie-this":
-      showMovieInfo();
+      showMovieInfo(inputParameter);
       break;
     case "do-what-it-says":
-      showSomeInfo();
+      showSomeInfo(inputParameter);
       break;
   }
 }
@@ -49,20 +50,100 @@ function showSongsInfo(inputParameter) {
       }
       var songs = data.tracks.items;
       for (var i = 0; i < songs.length; i++) {
-        console.log("SONG INFO");
-        
-       
-        console.log("Song name: " + songs[i].name);
-        
-        console.log("Preview song: " + songs[i].preview_url);
-       
-        console.log("Album: " + songs[i].album.name);
-        
-        console.log("Artist(s): " + songs[i].artists[0].name);
-        
         console.log("*****************************");
-        
+
+        console.log("SONG INFO");
+
+        console.log("Song name: " + songs[i].name);
+
+        console.log("Preview song: " + songs[i].preview_url);
+
+        console.log("Album: " + songs[i].album.name);
+
+        console.log("Artist(s): " + songs[i].artists[0].name);
+
+        console.log("*****************************");
       }
     }
   );
+}
+// bands in town
+function showConcertInfo(inputParameter) {
+  if (inputParameter === undefined) {
+    // default songs
+    inputParameter = "Imagine Dragons - Birds";
+  }
+  
+  axios
+    //still havent fig
+
+    .get(
+      `https://rest.bandsintown.com/artists/${inputParameter}/events?app_id=codingbootcamp`,
+      
+      )
+
+    .then(function(response, err) {
+      if (err) {
+        console.log("Error orrurred:" + err);
+        return;
+      }
+      var data = response.data;
+      for (var i = 0; i < data.length; i++) {
+        var date = moment(data[i].datetime).calendar();
+
+        console.log(/--------------------------/);
+        
+        console.log("Venue Name: ", data[i].venue.name);
+        console.log(
+          "Venue Location: ",
+          data[i].venue.city + " " + data[i].venue.region
+        );
+        console.log("Date: ", date);
+        console.log(/--------------------------/);
+      }
+    });
+}
+// movie 
+function showMovieInfo(inputParameter) {
+
+  if (inputParameter === undefined) {
+    inputParameter = "Mr. Nobody";
+
+    console.log(
+      "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/"
+    );
+
+    console.log("It's on Netflix!");
+  }
+  
+  axios
+    .get(
+      `http://www.omdbapi.com/?i=tt3896198&apikey=${
+        process.env.OMDB_ID
+      }&t=${inputParameter}`
+      )
+    
+    .then(function(response,err) {
+      if(err){
+        console.log("Error orrurred:" + err);
+        return;
+      }
+      console.log("+++++++++++++++++++++++++++++++++");
+      
+      console.log("Title of the movie: ", response.data.Title);
+      console.log("Year the movie came out: ", response.data.Year);
+      console.log("IMDB Rating of the movie: ", response.data.Ratings[0].Value);
+      console.log(
+        "Rotten Tomatoes Rating of the movie: ",
+        response.data.Ratings[1].Value 
+      );
+      console.log(
+        "Country where the movie was produced: ",
+        response.data.Country
+      );
+      console.log("Language of the movie: ", response.data.Language);
+      console.log("Plot of the movie: ", response.data.Plot);
+      console.log("Actors in the movie: ", response.data.Actors);
+      console.log("+++++++++++++++++++++++++++++++++");
+    });
 }
